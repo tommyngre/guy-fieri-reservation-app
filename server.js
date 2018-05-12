3578abbe730947f9bbedc3438ee14805c210d582
unknown <tommyn.gre@gmail.com>
2018-05-12 11:56:00 -0400

    files

---
+++ server.js
@@ -0,0 +1,47 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+let path = require('path');
+
+let app = express();
+let PORT = 3200;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+let tables = [
+  {
+    name: "bimp",
+    phoneNumber: "919-900-9000",
+    email: "bimp@bimp.com",
+    num: "101"
+  }
+];
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+app.get("/table", function (req, res) {
+  res.sendFile(path.join(__dirname, "table.html"))
+});
+app.get("/reserve", function (req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"))
+});
+
+app.get("/api/tables", function (req, res) {
+  return res.json(tables);
+});
+app.post("/api/tables", function (req, res) {
+  let newTable = req.body;
+
+  console.log(newTable)
+
+  tables.push(newTable);
+
+  //what is this doing?
+  res.json(newTable);
+});
+
+
+app.listen(PORT, function () {
+  console.log("listening on port " + PORT);
+});
